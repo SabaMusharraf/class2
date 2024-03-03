5331a50a17e3370b39908c7f8fb1ad79d248f1b3
sabamusharraf <sabahussain337@gmail.com>
2024-03-03 17:36:36 +0500

    Add some codes

--- main.js
+++ main.js
@@ -2,3 +2,5 @@ var firstName = "Saba";
 var age = 30;
 console.log(firstName);
 console.log(age);
+var lastName = "Hussain";
+console.log(firstName + lastName);
--- main.ts
+++ main.ts
@@ -4,3 +4,6 @@ let age: number = 30;
 console.log(firstName);
 console.log(age);
 
+let lastName :string =  "Hussain";
+
+console.log(firstName +  lastName);
